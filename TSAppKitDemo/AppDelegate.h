0a01923d0f24a5f96e30777ffd1298b5beee9f2d
Tristan Seifert <tristan@squee.co>
2014-11-22 14:30:18 -0600

    Initial Commit

---
+++ TSAppKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TSAppKitDemo
+//
+//  Created by Tristan Seifert on 11/22/14.
+//  Copyright (c) 2014 Tristan Seifert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
